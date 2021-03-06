4e2e05b403b23af361196b98959e65119f6e67bb
hernan reyes <hernanryz@hotmail.com>
2021-03-06 16:11:30 -0600

    fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function iniciaMapa() {
     map = new google.maps.Map(document.getElementById("map"), propiedades);
 
     var icono = {
-        url: "https://media2.giphy.com/media/omHPYZttAVAAw/giphy.gif", 
+        url: "https://giphy.com/gifs/omHPYZttAVAAw/html5", 
         scaledSize: new google.maps.Size(50,50),
         origin: new google.maps.Point(0,0),
         anchor: new google.maps.Point(0,0)
